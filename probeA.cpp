9d4c4b22253328947ab013dc5c407610ed324756
chaneligans <chanelmdza@gmail.com>
2019-02-26 19:11:34 -0800

    fixed it

--- probeA.cpp
+++ probeA.cpp
@@ -37,32 +37,25 @@ int main() {
     int probeA;
     probeA = 997;
 
-    // send msg again
-    bool msgRecieved;
-    msgRecieved = true;
-
     // msg to send
     buf msg;
 	int size = sizeof(msg)-sizeof(long);
 
-    msg.mtype = 111;
+    msg.mtype = 11;
 
     // continue looping until the rand number is less than 100
     while (randNum >= 100) {
         if (randNum % probeA == 0) {
             strcpy(msg.greeting, "Probe A" + randNum);
-            if (msgRecieved) {
-                // send msg
-	            msgsnd(qid, (struct msgbuf *)&msg, size, 0);
-                msgRecieved = false;
-
-                // recieve msg
-                msgrcv(qid, (struct msgbuf *)&msg, size, 314, 0); // reading
-	            cout << getpid() << ": gets reply" << endl;
-	            cout << "reply: " << msg.greeting << endl;
-	            cout << getpid() << ": now exits" << endl;
-                msgRecieved = true;
-            }
+            // send msg
+	        msgsnd(qid, (struct msgbuf *)&msg, size, 0);
+            
+            // recieve msg
+            msgrcv(qid, (struct msgbuf *)&msg, size, 314, 0); // reading
+	        cout << getpid() << ": gets reply" << endl;
+	        cout << "reply: " << msg.greeting << endl;
+	        cout << getpid() << ": now exits" << endl;
+                       
         }
         randNum = rand();
     }
