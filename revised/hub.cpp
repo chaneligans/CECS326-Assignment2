3b15a00bfa2b5f9be9a27c16313bd6eead2b0232
Dinh Luong <luongydinh12@gmail.com>
2019-03-15 21:29:03 -0700

    Comment for hub.cpp

--- revised/hub.cpp
+++ revised/hub.cpp
@@ -4,6 +4,16 @@
 // Probe B mtype 12
 // Probe C mtype 13
 
+/*
+Created by Dinh Luong
+CECS 326 - Assignment #2
+File name: hub.cpp
+Description: The hub will receive data from 3 probes. Hub will
+             display the message and random number sent from a
+             probe (A, B or C). Hub ends when 3 probes have been
+             ended
+*/
+
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
@@ -16,7 +26,7 @@
 using namespace std;
 
 int main()
-{
+{ // Print out Hub's PID
    cout << "HUB's PID is " << getpid() << endl;
 
    // create my msgQ with key value from ftok()
@@ -25,13 +35,13 @@ int main()
    // declare my message buffer
    struct buf
    {
-      long mtype;        // required
-      char greeting[5]; // mesg content
-      int randInt;         // random number
+      long mtype;       // required
+      char greeting[5]; // message content
+      int randInt;      // random number
    };
 
-   buf msg;
-   int size = sizeof(msg) - sizeof(long);
+   buf msg;                               // create msg
+   int size = sizeof(msg) - sizeof(long); // define size
 
    //Boolean variable to keep track of which probe has been terminated
    bool endA = false;
@@ -46,21 +56,26 @@ int main()
    msgrcv(qid, (struct msgbuf *)&msg, size, 100, 0);
    pidB = msg.randInt;
 
-   while (endA == false || endB == false || endC == false)
+   while (endA == false || endB == false || endC == false) // Hub exits when all 3 probes exited
    {
-      msgrcv(qid, (struct msgbuf *)&msg, size, -14, 0);
-      counter += 1;
-      cout<<msg.greeting<< ": " <<msg.randInt<< " "<< counter<<endl;
+      msgrcv(qid, (struct msgbuf *)&msg, size, -14, 0);                      // receive message from a probe
+      counter += 1;                                                          // increase message counter
+      cout << msg.greeting << ": " << msg.randInt << " " << counter << endl; // Print out the message and random number
+
       //Check if the message from Probe A
       if (strncmp(msg.greeting, "A", 1) == 0 || strncmp(msg.greeting, "EndA", 4) == 0)
       {
-         cout << "Probe A sends a message to Hub: "<< msg.randInt << endl;
+         cout << "Probe A sends a message to Hub: " << msg.randInt << endl;
+
+         // Check if probe A exited
          if (strncmp(msg.greeting, "EndA", 4) == 0)
          {
             cout << "Probe A exits" << endl;
             endA = true;
-	    continue;
+            continue;
          } // Probe A has been terminated
+
+         // Hub replies probe A
          msg.mtype = 15;
          msg.randInt = 0;
          strncpy(msg.greeting, "Hub replies", size);
@@ -68,20 +83,23 @@ int main()
          msgsnd(qid, (struct msgbuf *)&msg, size, 0);
       }
 
+      // Check if message counter reaches 10000. If yes, force probe B to exit
       if (counter == 10000)
       {
-           cout << counter<<endl;
+         cout << counter << endl;
          force_patch(pidB); // Force_path file
-             endB = true;
-      } // Force B to exit
+         endB = true;
+      }
+
+      // Check if probe C exited
       if (strncmp(msg.greeting, "EndC", 4) == 0)
       {
          endC = true;
-      } // Probe C has been terminated by the user
+      }
    }
 
    cout << "Hub is terminating" << endl;
 
-   msgctl(qid, IPC_RMID, NULL);
+   msgctl(qid, IPC_RMID, NULL); // deallocate the queue
    exit(0);
 }
